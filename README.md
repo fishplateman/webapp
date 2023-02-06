6f1650db1d3e27453af7cad385e496131ce8ca55
“fishplateman” <android9s@icloud.com>
2023-02-05 20:38:05 -0500

    update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # webapp
 CSYE 6225 Spring 2023
+- clone the project to your computer
+- choose jdk 8
+- use maven to install the dependencies in the pom.xml
+- run main function in Application.java
---
+++ src/main/java/LeiYang/util/EmailVerify.java
@@ -0,0 +1,11 @@
+package LeiYang.util;
+import java.util.regex.Pattern;
+
+public class EmailVerify {
+    public static boolean isValidEmail(String email) {
+        if ((email != null) && (!email.isEmpty())) {
+            return Pattern.matches("^(\\w+([-.][A-Za-z0-9]+)*){3,18}@\\w+([-.][A-Za-z0-9]+)*\\.\\w+([-.][A-Za-z0-9]+)*$", email);
+        }
+        return false;
+    }
+}
